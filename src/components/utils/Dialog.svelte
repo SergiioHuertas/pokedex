c374fb9c7e17e616e17811c6475b818bbec286a8
sergiiohuertas <sergio.huertas@yapiko.com>
2023-10-17 13:02:55 +0200

    [SH-0] fix dialog position on mobile

--- src/components/utils/Dialog.svelte
+++ src/components/utils/Dialog.svelte
@@ -19,8 +19,25 @@
         font-family: 'Press Start 2P', cursive;
         line-height: 150%;
         width: 60%;
-        position: absolute;
-        top: 0;
-        bottom: 0;
+    }
+
+    @media screen and (min-width: 768px) {
+        .dialog-content {
+            display: flex;
+            background-color: rgba(0, 0, 0, 0.8);
+            color: white;
+            justify-content: center;
+            flex-direction: column;
+            align-items: center;
+            padding: 5px;
+            gap: 5%;
+            font-size: 12px;
+            font-family: 'Press Start 2P', cursive;
+            line-height: 150%;
+            width: 60%;
+            position: absolute;
+            top: 0;
+            bottom: 0;
+        }
     }
 </style>
